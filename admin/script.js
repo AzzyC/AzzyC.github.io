6d8870066be33297526febbfae682791b6c2fd1e
AzzyC <azmath2000@gmail.com>
2021-05-19 20:29:20 +0100

    admin: script: External script to display times
    - Scripting within html looks untidy
        ~ Created a subconscious space limitation to not write excessively within html as it would look be difficult to organise. In its own file, can improve functions

---
+++ admin/script.js
@@ -0,0 +1,35 @@
+function admintimes() {
+  var today = new Date();
+
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+// Pad the time with leading zeroes
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  var current = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("currentTime").innerHTML = current;
+
+// Incorrect time display if add 15 minutes to a time that is past 45th minute
+// Therefore, reverse the calculation by subtracting 45 minutes
+  if (minutes >= 45) {
+    hours++;
+    minutes = minutes - 45;
+  }
+  else
+    minutes = today.getMinutes() + 15;
+
+// Pad the time with leading zeroes
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+
+  var wait = hours + ":" + minutes;
+
+  document.getElementById("waitTime").innerHTML = wait;
+
+  document.title = 'ᴀᴅᴍɪɴ ┇ ' + current + ' ┇ ' + wait;
+}
